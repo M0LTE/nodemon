fbba40a77d7c7ccb28e331b12d55209ef752da0e
Tom M0LTE <tom@m0lte.uk>
2024-06-03 20:07:34 +0100

    Initial commit

---
+++ src/nodemon/Configuration/NodeMonConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace nodemon.Configuration;
+
+public class NodeMonConfig
+{
+    public class Port
+    {
+        public required string Id { get; set; }
+        public required int RelayPin { get; set; }
+        public required string RadioPort { get; set; }
+        public required int RadioBaud { get; set; }
+    }
+
+    public required string ArduinoPort { get; set; }
+    public required Port[] Ports { get; set; }
+}
---
+++ src/nodemon/Services/TaitMonitor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using nodemon.Configuration;
+
+namespace nodemon.Services;
+
+public class TaitMonitor(IOptions<NodeMonConfig> config, ILogger<TaitMonitor> logger) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
