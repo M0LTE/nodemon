2c552574ba8363286eeb605220c3bcb19ab03365
Tom M0LTE <tom@m0lte.uk>
2024-07-09 22:39:11 +0100

    Hack in some on/off buttons

--- src/nodemon/Services/NodeHub.cs
+++ src/nodemon/Services/NodeHub.cs
@@ -4,4 +4,21 @@ namespace nodemon.Services;
 
 public class NodeHub : Hub
 {
+    private readonly ILogger<NodeHub> logger;
+    private readonly ArduinoSingleton arduinoSingleton;
+
+    public NodeHub(ILogger<NodeHub> logger, ArduinoSingleton arduinoSingleton)
+    {
+        this.logger = logger;
+        this.arduinoSingleton = arduinoSingleton;
+    }
+
+
+    public Task ToggleChanged(int relay, bool isOn)
+    {
+        logger.LogInformation("ToggleChanged: {relay} {isOn}", relay, isOn);
+        arduinoSingleton.SetRelay(relay, isOn);
+
+        return Task.CompletedTask;
+    }
 }
--- src/nodemon/wwwroot/js/node.js
+++ src/nodemon/wwwroot/js/node.js
@@ -39,30 +39,21 @@ connection.on("RssiUpdate", function (rssi) {
     }
     lineChart.update();
 });
-connection.on("ReceiveMessage", function (message) {
-    return
-    /*var li = document.createElement("li");
-    var list = document.getElementById("messagesList")
-    list.appendChild(li);
-
-    if (list.childNodes.length > 1000) {
-        list.removeChild(list.firstChild)
-    }
-
-    li.innerHTML = `<pre>${message.timestamp} ${message.modemId} ${htmlEncode(message.data)}</pre>`;
-    if (message.direction == '>') {
-        li.style.color = 'red'
-    } else if (message.direction == '<') {
-        if (message.data.includes(' > GB7RDG')) { // sorry
-            li.style.color = 'green'
-        } else {
-            li.style.color = 'gray'
-        }
-    }
-
-    $("html, body").animate({ scrollTop: $(document).height() }, 50);*/
-});
 
 connection.start().catch(function (err) {
     return console.error(err.toString());
+});
+
+$(document).ready(function () {
+    $('#toggle1').change(function () {
+        connection.invoke("ToggleChanged", 7, this.checked).catch(function (err) {
+            return console.error(err.toString());
+        })
+    });
+
+    $('#toggle2').change(function () {
+        connection.invoke("ToggleChanged", 6, this.checked).catch(function (err) {
+            return console.error(err.toString());
+        })
+    });
 });
