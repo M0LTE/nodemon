03f194f3c5709dec7cd54b95b0a034f9632d14c8
Tom M0LTE <tom@m0lte.uk>
2024-09-15 22:28:56 +0100

    Update TaitManager.cs

--- src/nodemon/Services/TaitManager.cs
+++ src/nodemon/Services/TaitManager.cs
@@ -28,7 +28,7 @@ public class TaitManager(IOptions<NodeMonConfig> config, ILogger<TaitManager> lo
         }
     }
 
-    private Stopwatch lastRssiReportedToUI = Stopwatch.StartNew();
+    private static Stopwatch lastRssiReportedToUI = Stopwatch.StartNew();
 
     private Task Run(NodeMonConfig.Port port)
     {
